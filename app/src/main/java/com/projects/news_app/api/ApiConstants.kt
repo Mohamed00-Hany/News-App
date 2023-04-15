6d2f88dac589977676da37a99b237bcc1ed90897
Mohamed Hany <mh4260160@gmail.com>
2023-04-15 02:05:58 +0200

    inhancement

--- app/src/main/java/com/projects/news_app/api/ApiConstants.kt
+++ app/src/main/java/com/projects/news_app/api/ApiConstants.kt
@@ -1,5 +1,7 @@
 package com.projects.news_app.api
 
 object ApiConstants {
-    const val apiKey="5a63010841e0405ea49cf82e0f969712"
+    //3fbe1730499c44029a7b7b9a7402cb44
+    //5a63010841e0405ea49cf82e0f969712
+    const val apiKey="3fbe1730499c44029a7b7b9a7402cb44"
 }
